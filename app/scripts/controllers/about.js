3747f629dfaf6e404d758745f285d2a8ff88e02e
danielindie95 <danielindie95@gmail.com>
2017-06-19 12:10:11 +0300

    yo angular seed

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hadaAdminApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hadaAdminApp
+ */
+angular.module('hadaAdminApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hadaAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hadaAdminApp
+ */
+angular.module('hadaAdminApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
